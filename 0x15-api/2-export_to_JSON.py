900f43c13534f330e15fa37b0decdbb44a709723
yismaw17 <yismawmulaw@yahoo.com>
2023-04-25 07:22:44 +0000

    task api

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,24 +1,28 @@
 #!/usr/bin/python3
-"""fetches information from JSONplaceholder API and exports to JSON"""
+"""
+Uses https://jsonplaceholder.typicode.com REST API and a given employee ID to
+record all tasks that are owned by this employee in a JSON file
+"""
 
-from json import dump
-from requests import get
+import json
+import requests
 from sys import argv
 
-
 if __name__ == "__main__":
-    todo_url = "https://jsonplaceholder.typicode.com/user/{}/todos".format(
-        argv[1])
-    name_url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
-    todo_result = get(todo_url).json()
-    name_result = get(name_url).json()
-
-    todo_list = []
-    for todo in todo_result:
-        todo_dict = {}
-        todo_dict.update({"task": todo.get("title"), "completed": todo.get(
-            "completed"), "username": name_result.get("username")})
-        todo_list.append(todo_dict)
-
-    with open("{}.json".format(argv[1]), 'w') as f:
-        dump({argv[1]: todo_list}, f)
+    userID = argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userID)).json()
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
+                         .format(userID)).json()
+    username = user.get('username')
+    tasks = []
+    for task in todos:
+        task_dict = {}
+        task_dict["task"] = task.get('title')
+        task_dict["completed"] = task.get('completed')
+        task_dict["username"] = username
+        tasks.append(task_dict)
+    jsonobj = {}
+    jsonobj[userID] = tasks
+    with open("{}.json".format(userID), 'w') as jsonfile:
+        json.dump(jsonobj, jsonfile)
